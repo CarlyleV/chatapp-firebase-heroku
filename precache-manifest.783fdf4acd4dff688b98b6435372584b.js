8e5103d21a1f1dc2153b6dc4236a118f3364b80b
CarlyleV <85601247+CarlyleV@users.noreply.github.com>
2021-08-20 17:58:09 +0900

    Add files via upload

---
+++ precache-manifest.783fdf4acd4dff688b98b6435372584b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dbf1f299b2b59b2ee3",
+    "url": "/css/app.c990ba7f.css"
+  },
+  {
+    "revision": "e04dc970d6545fc2cfe515094b2f3030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8dbf1f299b2b59b2ee3",
+    "url": "/js/app.48736b7a.js"
+  },
+  {
+    "revision": "50983a873e5593c9d8d0",
+    "url": "/js/chunk-vendors.324d7ae0.js"
+  },
+  {
+    "revision": "c3d91bbf7e7078330b108e648ae1562d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
